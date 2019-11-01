a6eeb6552effb4eeb155b563721aeac6003a28d6
Peter Kuma <peter.kuma@pg.canterbury.ac.nz>
2019-11-01 21:33:05 +1300

    setup.py update details and dep on pst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os.path
 setup(
 	name='alcf',
 	version='1.0.0',
-	description='Command line tool for processing of automatic lidar and ceilometer data and comparison with atmospheric models',
+	description='Automatic Lidar and Ceilometer Framework - processing of ALC data and lidar simulator',
 	author='Peter Kuma',
 	author_email='peter.kuma@fastmail.com',
 	license='MIT',
@@ -16,21 +16,22 @@ setup(
 	package_data={'alcf': [
 		os.path.join('..',p,f)
 		for p,_,fs in os.walk('alcf/opt')
-	 	for f in fs
+		for f in fs
 	]},
 	install_requires=[
 		'netCDF4>=1.2.9',
 		'cl2nc',
+		'pst',
 	],
-	keywords=['alc', 'ceilometer', 'lidar', 'atmospheric', 'model', 'nwp', 'gcm', 'cosp', 'actsim', 'vaisala', 'cl51', 'cl31', 'lufft', 'chm-15k', 'amsp', 'cmip5'],
+	keywords=['alc', 'ceilometer', 'lidar', 'atmosphere', 'model', 'simulator', 'nwp', 'gcm', 'cosp', 'actsim', 'vaisala', 'cl51', 'cl31', 'lufft', 'chm-15k', 'minimpl', 'amps', 'merra-2', 'um'],
 	url='https://github.com/peterkuma/alcf',
 	classifiers=[
-		'Development Status :: 2 - Pre-Alpha',
+		'Development Status :: 4 - Beta',
 		'Environment :: Console',
 		'Intended Audience :: Science/Research',
 		'License :: OSI Approved :: MIT License',
 		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
 		'Topic :: Scientific/Engineering :: Atmospheric Science',
 	]
 )
