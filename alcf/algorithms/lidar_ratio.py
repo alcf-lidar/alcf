Fix calculation of lidar ratio

alcf/algorithms/lidar_ratio.py
@@ -5,19 +5,18 @@ def lidar_ratio(d, eta=1.):
 	dz = np.diff([0] + list(d['zfull']))
 	if len(d['backscatter'].shape) == 3:
 		n, m, l = d['backscatter'].shape
-		lr = np.zeros((len(d['time']), l), dtype=np.float64)
+		bint = np.zeros((len(d['time']), l), dtype=np.float64)
 		dims = ['time', 'column']
 		for j in range(l):
 			for i in range(len(d['time'])):
-		 		lr[i,j] = np.sum(d['backscatter'][i,:,j]*dz)
+		 		bint[i,j] = np.sum(d['backscatter'][i,:,j]*dz)
 	else:
 		n, m = d['backscatter'].shape
-		lr = np.zeros(n, dtype=np.float64)
+		bint = np.zeros(n, dtype=np.float64)
 		dims = ['time']
 		for i in range(n):
-		 	lr[i] = np.sum(d['backscatter'][i,:]*dz)
-	lr = 2.*eta/lr
-	d['lr'] = lr
+		 	bint[i] = np.sum(d['backscatter'][i,:]*dz)
+	d['lr'] = 1./(2.*eta*bint) # See O'Connor et al. (2004), Equation 6.
 	d['.']['lr'] = {
 		'.dims': dims,
 		'long_name': 'lidar_ratio',