2ba3417c1b814d23006ed19ac6d60352f6503416
Peter Kuma <peter@peterkuma.net>
2021-02-24 21:58:29 +0100

    Add an example to alcf simulate help

--- alcf/cmds/simulate.py
+++ alcf/cmds/simulate.py
@@ -162,6 +162,14 @@ Options:
 - `overlap: <overlap>`: Cloud overlap assumption in the SCOPS subcolumn
    generator. `maximum` for maximum overlap, `random` for random overlap, or
   `maximum-random` for maximum-random overlap. Default: `maximum-random`.
+
+Examples:
+
+Simulate a Vaisala CL51 instrument from model data in `alcf_merra2_model`
+previously extracted using `alcf model` and store the output in the direcctory
+`alcf_merra2_simulate`.
+
+    alcf simulate cl51 alcf_merra2_model alcf_merra2_simulate
 	"""
 	lidar = LIDARS.get(type_)
 	if lidar is None:
