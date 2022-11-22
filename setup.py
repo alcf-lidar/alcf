3bd93333d1873044dec03ea042d77dc62adb77a8
Peter Kuma <peter@peterkuma.net>
2022-11-22 14:40:57 +0100

    setup.py: Update version dependency for ds-format

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
 		'netCDF4>=1.2.9',
 		'cl2nc>=3.3.0',
 		'aquarius-time>=0.1.0',
-		'ds-format>=1.0.0',
+		'ds-format>=3.4.0',
 		'pst-format>=2.0.0',
 		'astropy>=3.1.2',
 		'cftime>=1.5.1',
