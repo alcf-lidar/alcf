e457a8c619e92f21219307952366aaa14267946d
Peter Kuma <peter@peterkuma.net>
2024-04-02 13:30:34 +0100

    alcf model: Improve documentation

--- alcf/cmds/model.py
+++ alcf/cmds/model.py
@@ -151,8 +151,8 @@ Arguments
 - `type`: Input data type (see Types below).
 - `input`: Input directory.
 - `output`: Output directory.
-- `lon`: Point longitude.
-- `lat`: Point latitutde.
+- `lon`: Point longitude (degrees East).
+- `lat`: Point latitutde (degrees North).
 - `start`: Start time (see Time format below).
 - `end`: End time (see Time format below).
 - `track: <file>`, `track: { <file>... }`: One or more track NetCDF files (see Files below). If multiple files are supplied and `time_bnds` is not present in the files, they are assumed to be multiple segments of a discontinous track unless the last and first time of adjacent tracks are the same.
