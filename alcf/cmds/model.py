889a60f3864b26ea0cfdd78930c93e3dbaa75f92
Peter Kuma <peter@peterkuma.net>
2024-05-07 19:28:52 +0000

    alcf model: Limit default njobs to 16

--- alcf/cmds/model.py
+++ alcf/cmds/model.py
@@ -127,7 +127,7 @@ Arguments
 Options
 -------
 
-- `njobs: <n>`: Number of parallel jobs. Default: number of CPU cores.
+- `njobs: <n>`: Number of parallel jobs. Default: number of CPU cores capped to 16.
 - `-r`: Process the input directory recursively.
 - `--track_lon_180`: Expect track longitude between -180 and 180 degrees. This option is no longer needed as the conversion is automatically. [deprecated]
 - `override_year: <year>`: Override year in the track. Use if comparing observations with a model statistically and the model output does not have a corresponding year available. The observation time is converted to the same time relative to the start of the year in the specified year. Note that if the original year is a leap year and the override year is not, as a consequence of the above 31 December is mapped to 1 January. The output retains the original year as in the track, even though the model data come from the override year. Default: `none`.
@@ -173,7 +173,8 @@ Extract MERRA-2 model data in `M2I3NVASM.5.12.4` at 45 S, 170 E between 1 and 2 
 	if model is None:
 		raise ValueError('Invalid type: %s' % type_)
 
-	if njobs is None: njobs = os.cpu_count()
+	if njobs is None:
+		njobs = min(16, os.cpu_count())
 
 	warnings = []
 	index = None
